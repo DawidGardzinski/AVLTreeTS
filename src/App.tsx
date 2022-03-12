db874dd6143d5512dd654622964aec624589e9f3
Dawid <dawidgardzinski@gmail.com>
2022-03-12 14:55:56 +0100

    basic configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import TestComponent from "./Components/TestComponent/TestComponent";
+import { theme } from "./theme";
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <TestComponent />
+        </ThemeProvider>
+    );
+}
+
+export default App;
